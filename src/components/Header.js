e891a842f81247bb6fc3c0fbe332ea5a831fb129
Krins <krinskumar@gmail.com>
2023-05-11 23:23:10 -0400

    added-socials

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,12 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-      <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+      <header className='fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
         <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
           <Link to={'/'} className='max-w-[200px]'>
             <img src={Logo} alt='logo'/>
           </Link>
-          <nav className='hidden lg:flex gap-x-12'>
+          <nav className='hidden xl:flex gap-x-12'>
             <Link to='/' className='text-[#696c6d] hover:text-primary transition'>
               Home
             </Link>
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,9 +1,19 @@
 import React from 'react';
+//icons
+import { ImGithub, ImLinkedin,  } from 'react-icons/im';
+
 
 const Socials = () => {
   return (
     <div className='hidden xl:flex ml-24'>
-      Socials
+      <ul className='flex gap-x-4'>
+        <li className=''><a target='_blank' href='https://www.linkedin.com/in/krins/'>
+          <ImLinkedin className='text-3xl text-primary hover:text-[#0a63bc] transition' />
+        </a></li>
+        <li className=''><a target='_blank' href='https://github.com/KrinsKumar'>
+          <ImGithub className='text-3xl text-primary hover:text-[#696c6d] transition' />
+        </a></li>
+      </ul>
     </div>
   );
 };
