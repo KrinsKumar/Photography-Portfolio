b5bda7412d99b01f27306cbee821995e50abbdeb
Krins <krinskumar@gmail.com>
2023-05-13 00:10:57 -0400

    corrected-a-error

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -7,10 +7,10 @@ const Socials = () => {
   return (
     <div className='hidden xl:flex ml-24'>
       <ul className='flex gap-x-4'>
-        <li className=''><a target='_blank' href='https://www.linkedin.com/in/krins/'>
+        <li className=''><a rel="noreferrer" target='_blank' href='https://www.linkedin.com/in/krins/'>
           <ImLinkedin className='text-3xl text-primary hover:text-[#0a63bc] transition' />
         </a></li>
-        <li className=''><a target='_blank' href='https://github.com/KrinsKumar'>
+        <li className=''><a rel="noreferrer" target='_blank' href='https://github.com/KrinsKumar'>
           <ImGithub className='text-3xl text-primary hover:text-[#696c6d] transition' />
         </a></li>
       </ul>
